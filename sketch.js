73d4a8f8c809d57f39e52d2de753824ad0901bb5
Angelina9896 <86651677+Angelina9896@users.noreply.github.com>
2021-07-21 12:23:56 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+var garden,rabbit;
+var gardenImg,rabbitImg;
+var apple,leaf
+var appleImg,leafImg
+
+function preload(){
+  gardenImg = loadImage("garden.png");
+  rabbitImg = loadImage("rabbit.png");
+  appleImg=loadImage("apple.png")
+  leafImg=loadImage("leaf.png")
+  
+}
+
+function setup(){
+  
+  createCanvas(400,400);
+  rabbit.collide(edges);
+  
+// Moving background
+garden=createSprite(200,200);
+garden.addImage(gardenImg);
+apple=createSprite()
+apple.addImage(appleImg)
+
+//creating boy running
+rabbit = createSprite(180,340,30,30);
+rabbit.scale =0.09;
+rabbit.addImage(rabbitImg);
+leaf=createSprite()
+leaf.addImage(leafImg)
+}
+
+
+function draw() {
+  background(0);
+ 
+  rabbit=World.MouseX
+  
+  edges= createEdgeSprites();
+ 
+
+  drawSprites();
+}
